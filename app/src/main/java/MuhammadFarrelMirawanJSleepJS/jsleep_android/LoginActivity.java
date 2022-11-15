cbccd7ac33091706a1cd812a8887ccbc54191807
farrelmrwn <muhammad.farrel13@ui.ac.id>
2022-11-15 17:55:59 +0700

    CS Modul 8

---
+++ app/src/main/java/MuhammadFarrelMirawanJSleepJS/jsleep_android/LoginActivity.java
@@ -0,0 +1,35 @@
+package MuhammadFarrelMirawanJSleepJS.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.RegisterClick);
+        Button  main = findViewById(R.id.loginButton);
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+        main.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+
+}
